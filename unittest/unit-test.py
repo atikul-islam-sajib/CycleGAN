4b28c8f14da3dd889bc764c5909bf437465a7c01
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-04-24 14:31:32 +0200

    netG code is passed the unittest

--- unittest/unit-test.py
+++ unittest/unit-test.py
@@ -6,6 +6,7 @@ import torch
 sys.path.append("src/")
 
 from utils import load, params
+from generator import Generator
 
 
 class UnitTest(unittest.TestCase):
@@ -19,6 +20,7 @@ class UnitTest(unittest.TestCase):
         self.test_dataloader = load(
             os.path.join(params()["path"]["processed_path"], "test_dataloader.pkl")
         )
+        self.netG = Generator(in_channels=3)
 
     def test_data_quantity(self):
         self.assertEqual(sum(image.size(0) for image, _ in self.dataloader), 18)
@@ -30,6 +32,11 @@ class UnitTest(unittest.TestCase):
     def test_test_dataloader(self):
         data, _ = next(iter(self.test_dataloader))
         self.assertEqual(data.size(), torch.Size([4, 3, 256, 256]))
+
+    def test_netG_output(self):
+        self.assertEqual(
+            self.netG(torch.randn(1, 3, 256, 256)).size(), torch.Size([1, 3, 256, 256])
+        )
 
 
 if __name__ == "__main__":
