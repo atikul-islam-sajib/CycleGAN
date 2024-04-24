bc24fdbfb5889e23b0c019db65d8458a5fe182f2
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-04-25 01:01:44 +0200

    doc strings is added in GANLoss #22

--- src/loss/gan_loss.py
+++ src/loss/gan_loss.py
@@ -4,13 +4,44 @@ import torch.nn as nn
 
 
 class GANLoss(nn.Module):
+    """
+    Custom GAN Loss module that calculates the Mean Squared Error loss between
+    the predicted and actual tensor values.
+
+    Attributes:
+        reduction (str): Specifies the reduction to apply to the output: 'none', 'mean' or 'sum'.
+        loss (torch.nn.modules.loss): The MSE Loss instance with specified reduction.
+
+    Methods:
+        forward(predicted, actual): Computes the MSE loss between predicted and actual values.
+    """
+
     def __init__(self, reduction="mean"):
+        """
+        Initializes the GANLoss module with the specified reduction method.
+
+        Args:
+            reduction (str): The method used to reduce the MSE loss over the batch. Defaults to "mean".
+        """
         super(GANLoss, self).__init__()
 
         self.reduction = reduction
         self.loss = nn.MSELoss(reduction=self.reduction)
 
     def forward(self, predicted, actual):
+        """
+        Forward pass of the loss module to compute the MSE loss.
+
+        Args:
+            predicted (torch.Tensor): The predicted outputs from the model.
+            actual (torch.Tensor): The actual ground truth values.
+
+        Returns:
+            torch.Tensor: The computed MSE loss as per the specified reduction method.
+
+        Raises:
+            ValueError: If inputs are not torch tensors.
+        """
         if type(predicted) == torch.Tensor and type(actual) == torch.Tensor:
             return self.loss(predicted, actual)
         else:
