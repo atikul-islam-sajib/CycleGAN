bddc825874e0ec8376ef06dfa25c27620328b72d
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-04-26 22:55:27 +0200

    test code is added in the src

---
+++ src/test.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import torch
+import torch.nn as nn
+
+
+class TestModel:
+    def __init__(self, device="mps"):
+        self.device = device
+
+    def select_best_model(self):
+        pass
+
+    def plot(self):
+        pass
+
+    def test(self):
+        pass
